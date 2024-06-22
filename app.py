ba08a470cba7055bed48dc31a30db97d2d61b946
Deep99739 <142610132+Deep99739@users.noreply.github.com>
2024-06-22 22:48:02 +0530

    Updated api error

--- app.py
+++ app.py
@@ -10,26 +10,30 @@ def Index():
 	return render_template("index.html")
 
 
+
+
 @app.route("/Summarize", methods=["GET", "POST"])
 def Summarize():
 	if req.method == "POST":
 		API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-		headers = {"Authorization": f"Bearer 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"}
-
+		headers = {"Authorization": f"Bearer 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"}
 		data = req.form["data"]
-
-		maxL = int(req.form["maxL"])
-		minL = maxL // 4
-
 		def query(payload):
 			response = requests.post(API_URL, headers=headers, json=payload)
 			return response.json()
+		maxL = int(req.form["maxL"])
+		minL = maxL // 4
 
-		output = query({
+		
+		payload = {
 			"inputs": data,
-			"parameters": {"min_length": minL, "max_length": maxL},
-		})[0]
-
+			"parameters": {"min_length": minL, "max_length": maxL}
+		}
+		output = query(payload)[0]
+		
+		# print("hello")
+		# print(output)
+		# print("help")
 		return render_template("index.html", result=output["summary_text"])
 	else:
 		return render_template("index.html")
